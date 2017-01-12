aaf6912c3c7eea7ecf4be62264079f6eea91d04b
Mark Muskardin <mark.muskardin@gmail.com>
2017-01-11 22:32:22 -0800

    Added AppBar component

--- src/containers/Header/index.js
+++ src/containers/Header/index.js
@@ -1,7 +1,7 @@
 import React, { Component }   from 'react';
 import { connect }            from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { AppBar }             from 'material-ui';
+import { AppBar }             from 'components/AppBar';
 
 /* actions */
 import * as uiActionCreators from 'core/actions/actions-ui';
