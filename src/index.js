615b5785c9936883c701d1d856909c00c82d41b4
Mark Muskardin <mark.muskardin@gmail.com>
2017-01-14 12:55:10 -0800

    changed history object code in index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import React                        from 'react';
-import ReactDOM                     from 'react-dom';
-import { Provider }                 from 'react-redux';
-import { Router, useRouterHistory } from 'react-router';
-import { createHashHistory }        from 'history';
-import configureStore               from 'core/store/configureStore';
-import routes                       from './routes';
+import React                   from 'react';
+import ReactDOM                from 'react-dom';
+import { Provider }            from 'react-redux';
+import { Router, hashHistory } from 'react-router';
+import { createHashHistory }   from 'history';
+import configureStore          from 'core/store/configureStore';
+import routes                  from './routes';
 
-const history = useRouterHistory(createHashHistory)({ queryKey: false });
 const store   = configureStore();
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router history={history} routes={routes} />
+    <Router history={hashHistory} routes={routes} />
   </Provider>,
   document.getElementById('root')
 );
