623cdede88200c2928ef465a0fb22f04413be815
ihsansatriawan <ihsan.satriawan.20@gmail.com>
2018-02-14 18:41:43 +0700

    Add form login

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
 
 /* component styles */
 import { styles } from './styles.scss';
@@ -11,7 +13,20 @@ export default class Login extends Component {
   render() {
     return (
       <div className={styles}>
-        LOGIN HERE
+        <TextField
+          hintText="Enter your Username"
+          floatingLabelText="Username"
+          onChange = {(event,newValue) => this.setState({username:newValue})}
+          />
+        <br/>
+        <TextField
+          type="password"
+          hintText="Enter your Password"
+          floatingLabelText="Password"
+          onChange = {(event,newValue) => this.setState({password:newValue})}
+          />
+        <br/>
+        <RaisedButton label="Submit" primary={true} style={styles} onClick={(event) => this.handleClick(event)}/>
       </div>
     );
   }
