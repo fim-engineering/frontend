8e879c9088d9a0ad48bc298e1f99791da2c5bd64
ihsansatriawan <ihsan.satriawan.20@gmail.com>
2018-02-17 17:53:09 +0700

    Fix eslint

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -73,7 +73,7 @@ class Login extends Component {
         actions.ui.toggleProgressbar(false);
         this.toggleDisableButton()
       })
-      .catch(_ => {
+      .catch(() => {
         this.showToaster('Gagal Login')
         actions.ui.toggleProgressbar(false);
         this.toggleDisableButton()
