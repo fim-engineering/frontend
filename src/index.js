f84ca9ec680b53f2b457d2467a6839fccce23579
ihsansatriawan <ihsan.satriawan.20@gmail.com>
2018-02-17 22:08:48 +0700

    Add raven js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import { Provider }   from 'react-redux';
 import configureStore from 'core/store/configureStore';
 import App            from 'containers/App';
 import history1 from './core/browserHistory.js';
-
+import Raven from 'raven-js';
+Raven.config('https://05b0d00bc32b40d5b31031e2e4afb931@sentry.io/289884').install()
 
 const store = configureStore();
 
