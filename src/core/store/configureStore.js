4ce07f4e4fcbcc62122db6a50284ca5c30d8277d
ihsansatriawan <ihsan.satriawan.20@gmail.com>
2018-02-27 11:40:00 +0700

    Disabled redux storage

--- src/core/store/configureStore.js
+++ src/core/store/configureStore.js
@@ -14,19 +14,19 @@ import debounce from 'redux-storage-decorator-debounce'
 
 export default function configureStore(initialState) {
   /* - START - SETUP SAVE REDUX TO LOCALSTORAGE */
-  storage.reducer(combineReducers(rootReducer));
-  // const reducer = storage.reducer(combineReducers(rootReducer));
-  const prefixLocalStorage = 'fim'
-  let engine = createEngine(prefixLocalStorage);
+  // storage.reducer(combineReducers(rootReducer));
+  // // const reducer = storage.reducer(combineReducers(rootReducer));
+  // const prefixLocalStorage = 'fim'
+  // let engine = createEngine(prefixLocalStorage);
 
-  const filterOptions = [
-    'whitelisted-key',
-    ['user']
-  ]
+  // const filterOptions = [
+  //   'whitelisted-key',
+  //   ['user']
+  // ]
 
-  engine = filter(engine, filterOptions);
-  engine = debounce(engine, 10);
-  const middlewareStorage = storage.createMiddleware(engine);
+  // engine = filter(engine, filterOptions);
+  // engine = debounce(engine, 10);
+  // const middlewareStorage = storage.createMiddleware(engine);
   /* - END - SETUP SAVE REDUX TO LOCALSTORAGE */
 
 
@@ -37,23 +37,23 @@ export default function configureStore(initialState) {
     process.env.NODE_ENV === 'development'
   });
 
-  const middleware = applyMiddleware(reduxThunk, routerMiddleware(history), logger, middlewareStorage);
+  const middleware = applyMiddleware(reduxThunk, routerMiddleware(history), logger);
 
   const store = middleware(createStore)(
     rootReducer,
     DEV && window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
     initialState);
 
-  const load = storage.createLoader(engine);
+  // const load = storage.createLoader(engine);
 
-  /* - START - SETUP LOAD REDUX FROM LOCALSTORAGE */
-  const cachedStore = typeof window !== 'undefined'
-    ? !!localStorage.getItem(prefixLocalStorage)
-    : false;
+  // /* - START - SETUP LOAD REDUX FROM LOCALSTORAGE */
+  // const cachedStore = typeof window !== 'undefined'
+  //   ? !!localStorage.getItem(prefixLocalStorage)
+  //   : false;
 
-  if (cachedStore) {
-    load(store)
-  }
+  // if (cachedStore) {
+  //   load(store)
+  // }
   /* - END - SETUP LOAD REDUX FROM LOCALSTORAGE */
 
   if (module.hot) {
