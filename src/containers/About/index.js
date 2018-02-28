8edf58aa4481f44558664754518bebbde1c1722a
ihsansatriawan <ihsan.satriawan.20@gmail.com>
2018-02-28 19:45:37 +0700

    Add about

--- src/containers/About/index.js
+++ src/containers/About/index.js
@@ -11,7 +11,26 @@ export default class About extends Component {
   render() {
     return (
       <div className={styles}>
-        LALALAL
+      <p>
+      Portal Sistem Informasi FIM dibangun sejak tahun 2013, awalnya sebagai portal pendaftaran peserta FIM 14.
+      </p>
+
+      <p>
+      Saat ini, portal direncanakan untuk menjadi pusat data bagi organisasi FIM secara keseluruhan, dan akan dibangun secara bertahap.
+      </p>
+
+      <p>
+      Kontributor Portal 2013 - 2018 :
+      </p>
+      <ul>
+        <li>Pahlevi Fikri Aulia</li>
+        <li>Ihsan Satriawan</li>
+        <li>Almas Shabrina</li>
+        <li>Febrian Imanda</li>
+        <li>Bagus Dwi Utama</li>
+        <li>Alim</li>
+        <li>Habibie</li>
+      </ul>
       </div>
     );
   }
