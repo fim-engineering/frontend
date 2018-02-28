a82f3658a4022439fabf51ede000d2a77ac249e1
baguskah <dwiutamabagus@gmail.com>
2018-02-28 23:58:44 +0700

    change icon

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -16,6 +16,10 @@ import { styles } from './styles.scss';
 import * as uiActionCreators   from 'core/actions/actions-ui';
 import * as userActionCreators   from 'core/actions/actions-user';
 import ActionAndroid from 'material-ui/svg-icons/action/android';
+import RegIcon from 'material-ui/svg-icons/action/account-box';
+import ActIcon from 'material-ui/svg-icons/maps/local-activity';
+import PersIcon from 'material-ui/svg-icons/editor/bubble-chart';
+import MeandFIcon from 'material-ui/svg-icons/hardware/toys';
 import FlightTakeoff from 'material-ui/svg-icons/action/flight-takeoff';
 import {
   Step,
@@ -189,7 +193,7 @@ class Home extends Component {
                   disabled={isSubmitFinalSubmit}
                   primary={true}
                   onClick={this.handleRedirect('/profile')}
-                  icon={<ActionAndroid />}
+                  icon={<RegIcon />}
                   label="Isi Data Diri" fullWidth={false} />
                 {this.renderStepActions(0)}
               </StepContent>
@@ -203,7 +207,7 @@ class Home extends Component {
                   disabled={isSubmitFinalSubmit}
                   primary={true}
                   onClick={this.handleRedirect('/achievement')}
-                  icon={<FlightTakeoff />}
+                  icon={<ActIcon />}
                   label="Aktivitas dan Kepribadian" fullWidth={false} />
                 {this.renderStepActions(1)}
               </StepContent>
@@ -217,7 +221,7 @@ class Home extends Component {
                   disabled={isSubmitFinalSubmit}
                   primary={true}
                   onClick={this.handleRedirect('/personality')}
-                  icon={<FlightTakeoff />}
+                  icon={<PersIcon />}
                   label="Personality" fullWidth={false} />
                 {this.renderStepActions(2)}
               </StepContent>
@@ -231,7 +235,7 @@ class Home extends Component {
                   disabled={isSubmitFinalSubmit}
                   primary={true}
                   onClick={this.handleRedirect('/me-fim')}
-                  icon={<FlightTakeoff />}
+                  icon={<MeandFIcon />}
                   label="Tentang aku dan FIM" fullWidth={false} />
                 {this.renderStepActions(3)}
               </StepContent>
